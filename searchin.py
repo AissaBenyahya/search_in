e525884a05c0206f90e54454bfdd0f4ee8bedcaa
revo <ultimateaissa20@gmail.com>
2021-04-13 12:59:02 +0000

    added description

---
+++ searchin.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+print("""\
+
+    ### --------------------------------------------------------------------###
+    |   Version: 1.0                                                          |
+    |   Description: searchin is a command line searches inside a file        |
+    |   for a givin words by the user and returns number of matched           |
+    |   lines and their position in the file.                                 |
+    |   Licence: searchin is free software released under the "GNU General    |
+    |   Public License v3.0                                                   |
+    |   Copyright (c) 2021 Aissa Ben yahya - https://github.com/AissaBenyahya |
+    ###---------------------------------------------------------------------###
+    """)
